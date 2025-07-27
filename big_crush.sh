9fd57a3462d25b874dd2a4a453eccfe13f2e7ea5
Txangel <txangel@acm.org>
2025-07-27 16:47:11 +0100

    Upgrade again

--- big_crush.sh
+++ big_crush.sh
@@ -15,4 +15,7 @@ wg-quick down /tmp/generated_wg0.conf
 # shellcheck disable=SC2154
 cd "infra/${vendor}/"
 
+echo "[Info] Waiting for network to be stable before destroying instances..."
+sleep 10
+
 terraform destroy -auto-approve
